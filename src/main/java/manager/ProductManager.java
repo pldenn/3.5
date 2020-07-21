0e4d98c830514de49ce241bd739a237c5a86b8d8
Denis Timofeev <y719xy@gmail.com>
2020-07-21 17:40:11 +0300

    Add TShirt.java

--- src/main/java/manager/ProductManager.java
+++ src/main/java/manager/ProductManager.java
@@ -35,20 +35,14 @@ public class ProductManager {
             if (book.getName().equalsIgnoreCase(search)) {
                 return true;
             }
-            if (book.getAuthor().equalsIgnoreCase(search)) {
-                return true;
-            }
-            return false;
+            return book.getAuthor().equalsIgnoreCase(search);
         }
         if (product instanceof Smartphone) {
             Smartphone smartphone = (Smartphone) product;
             if (smartphone.getName().equalsIgnoreCase(search)) {
                 return true;
             }
-            if (smartphone.getManufacturer().equalsIgnoreCase(search)) {
-                return true;
-            }
-            return false;
+            return smartphone.getManufacturer().equalsIgnoreCase(search);
         }
         return false;
     }
